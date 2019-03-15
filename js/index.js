4a6ac4108b9088b027be40c953a9363b51c58818
DRobertson <dustinuriah@gmail.com>
2019-03-15 13:25:36 -0600

    Adjusted clear button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$( ".card" ).hide();
+$(".card").hide();
 
 var checkedItems = [];
 var dateSelect;
@@ -8,94 +8,125 @@ var genreSelect;
 var genreSelectCard;
 
 var d = new Date();
-var month = d.getMonth()+1;
+var month = d.getMonth() + 1;
 var day = d.getDate();
-var todaysDate = d.getFullYear() + '-' +
-    (month<10 ? '0' : '') + month + '-' +
-    (day<10 ? '0' : '') + day;
 
-function showAllDates() { 
-  $("[data-eventdate]").filter(function(){
+var todaysDate =
+    d.getFullYear() +
+    "-" +
+    (month < 10 ? "0" : "") +
+    month +
+    "-" +
+    (day < 10 ? "0" : "") +
+    day;
+
+function showAllDates() {
+  $("[data-eventdate]")
+    .filter(function() {
     return $(this).attr("data-eventdate") >= todaysDate;
-  }).show();
+  })
+    .show();
 }
 
-function hideOld() { 
-  $("[data-eventdate]").filter(function(){
+function hideOld() {
+  $("[data-eventdate]")
+    .filter(function() {
     return $(this).attr("data-eventdate") < todaysDate;
-  }).hide();
+  })
+    .hide();
+}
+
+function showDateEventMatch() {
+  $("input:checkbox[class=form-check-input]:checked").each(function() {
+    $(
+      ".card*[data-eventdate*=" +
+      dateSelect +
+      "]*[data-eventgenre*=" +
+      $(this).attr("id") +
+      "]"
+    ).show();
+    hideOld();
+  });
 }
 
 showAllDates();
 
 
-
 // datepicker
-$('.datepicker').datepicker({clearBtn: true}).on('changeDate', function(e){
-  dateSelect = e.date.getFullYear() + '-' + ("0" + (e.date.getMonth() + 1)).slice(-2) + '-' + ("0" + (e.date.getDate())).slice(-2);
-  dateSelectCard = $(".card*[data-eventdate*="+ dateSelect + "]");
+$(".datepicker")
+  .datepicker({ clearBtn: true })
+  .on("changeDate", function(e) {
+  dateSelect =
+    e.date.getFullYear() +
+    "-" +
+    ("0" + (e.date.getMonth() + 1)).slice(-2) +
+    "-" +
+    ("0" + e.date.getDate()).slice(-2);
+  dateSelectCard = $(".card*[data-eventdate*=" + dateSelect + "]");
 
-    $( ".card" ).hide();
-  
-    if ( checkedItems.length === 0 ) {
-        dateSelectCard.show();
-    } else {
-        $("input:checkbox[class=form-check-input]:checked").each(function(){
-            $(".card*[data-eventdate*="+ dateSelect + "]*[data-eventgenre*="+ $(this).attr("id") + "]").show();
-            hideOld();
-        })
-    }
+  $(".card").hide();
 
-  $( "p:first" ).html( dateSelect );
-  
-}).on('clearDate', function(e){
+  if (checkedItems.length === 0) {
+    dateSelectCard.show();
+  } else {
+    showDateEventMatch();
+  }
+
+  $("p:first").html(dateSelect);
+  console.log('Checked items: ' + checkedItems + ', Date selected: ' + dateSelect);
+
+})
+  .on("clearDate", function(e) {
   showAllDates();
   dateSelect = undefined;
-  $( "p:first" ).html( "No Date Selected" );
+  $("p:first").html("No Date Selected");
 });
 
 
-
 // genre checklist
-$('#music, #dance, #theatre').on('change', evt => {
+$(".form-check-input").on("change", evt => {
   genreSelect = evt.target.id;
-  genreSelectCard = $(".card*[data-eventgenre*="+ genreSelect + "]");
+  genreSelectCard = $(".card*[data-eventgenre*=" + genreSelect + "]");
 
-  filterUnchecked = $("input:checkbox[class=form-check-input]:not(:checked)").each(function(){
-    var notChecked = $(this).attr("id");
-    var genreSelectCard = $(".card*[data-eventgenre*="+ notChecked + "]").hide();
-  })
+  filterUnchecked = $(
+    "input:checkbox[class=form-check-input]:not(:checked)"
+  ).each(function() {
+    $(".card*[data-eventgenre*=" + $(this).attr("id") + "]").hide();
+  });
 
-  checkedItems = $("input:checkbox[class=form-check-input]:checked").map(function() {
+  checkedItems = $("input:checkbox[class=form-check-input]:checked")
+    .map(function() {
     return $(this).val();
-  }).get();
-  
-  if ( dateSelect === undefined ) {
-    $("input:checkbox[class=form-check-input]:checked").each(function(){
-      $(".card*[data-eventgenre*="+ $(this).attr("id") + "]").show();
+  })
+    .get();
+
+  if (dateSelect === undefined) {
+    $("input:checkbox[class=form-check-input]:checked").each(function() {
+      $(".card*[data-eventgenre*=" + $(this).attr("id") + "]").show();
       hideOld();
-      console.log(dateSelect)
-    })
+    });
   } else {
-    $("input:checkbox[class=form-check-input]:checked").each(function(){
-      $(".card*[data-eventdate*="+ dateSelect + "]*[data-eventgenre*="+ $(this).attr("id") + "]").show();
-      hideOld();
-    })
+    showDateEventMatch();
   }
 
-  if ( checkedItems.length === 0 || checkedItems.length === undefined ) {
-    if ( dateSelect == undefined ){
+  if (checkedItems.length === 0 || checkedItems.length === undefined) {
+    if (dateSelect == undefined) {
       showAllDates();
-      console.log("No genre or date selected - All events shown")
     } else {
       dateSelectCard.show();
-      console.log("No genre selected - All selected date events shown")
     }
   }
-  
-  console.log(checkedItems)
+  console.log('Checked items: ' + checkedItems + ', Date selected: ' + dateSelect);
+});
 
-})
+$( ".clear" ).click(function() {
+  $( ".form-check-input" ).prop( "checked", false );
+  dateSelect = undefined;
+  checkedItems = [];
+  showAllDates();
+  $("p:first").html("No Date Selected");
+  console.log('Checked items: ' + checkedItems + ', Date selected: ' + dateSelect);
+});
 
 
-console.log(checkedItems)
+console.log('Checked items: ' + checkedItems + ', Date selected: ' + dateSelect);
